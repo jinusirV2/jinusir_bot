b062a377568a73678b9f9fd8146eed027864ea52
jinusirV2 <94420294+jinusirV2@users.noreply.github.com>
2021-11-17 13:07:39 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -10,14 +10,14 @@ Asena.addCommand({pattern: 'menu', fromMe: false, desc: Lang.WP}, (async (messag
     var r_text = new Array ();
     
     
-    r_text[0] = "https://www.pixelstalk.net/wp-content/uploads/2016/03/Pikachu-wallpaper-HD-dowload.jpg";
+    r_text[0] = "https://i.ibb.co/8X1zZZG/Pics-Art-11-16-05-50-00.jpg";
     
     
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `⛦━━━━━✨️Ameer Suhail✨️━━━━━⛦
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `⛦━━━━━✨️JINU SIR✨️━━━━━⛦
 
 
 ━━━━━⛦
