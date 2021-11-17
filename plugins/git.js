56a4526e0a42fe8d7f51e58c94f14d49fbfcabfb
jinusirV2 <94420294+jinusirV2@users.noreply.github.com>
2021-11-17 20:39:23 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -7,7 +7,7 @@ const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
-    var skl = await axios.get("https://images.wallpapersden.com/image/download/pokemon-detective-pikachu-4k_a2lrZmWUmZqaraWkpJRsZ2WtbGdl.jpg", { responseType: 'arraybuffer' })
+    var skl = await axios.get("https://i.ibb.co/hWQ83gj/20211117-202942.jpg", { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*THIS BOT CREATED BY JINUSIR*
 ─────────────────────────────────────────────────────────────────
@@ -21,7 +21,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the
 
 *JINUSIR MOVIE WEBSITE: www.jinmovies.ga*
 
-
+       *JINUSIR BOT V.3 COMING SOON*
 
 
 `}) 
