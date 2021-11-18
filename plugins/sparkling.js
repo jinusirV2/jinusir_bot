5f753b89f5ce80dfb2e55f457cb48f63f9096f08
jinusirV2 <94420294+jinusirV2@users.noreply.github.com>
2021-11-18 08:10:11 +0530

    Update sparkling.js

--- plugins/sparkling.js
+++ plugins/sparkling.js
@@ -1,4 +1,4 @@
-/* Copyright (C) 2021 ameer-kallumthodi.
+/* Copyright (C) 2021 jinusir-jinu.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 JINUSIR*/
